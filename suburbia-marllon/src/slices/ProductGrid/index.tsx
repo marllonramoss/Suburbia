291c41728ad49999d4b6805378fb844b267ad26b
Marllon Ramos <marllonsteam@gmail.com>
2025-06-03 07:25:17 -0700

    skateboard section

---
+++ suburbia-marllon/src/slices/ProductGrid/index.tsx
@@ -0,0 +1,68 @@
+import { FC } from "react";
+import { Content, isFilled } from "@prismicio/client";
+import { PrismicRichText, PrismicText, SliceComponentProps } from "@prismicio/react";
+import { Bounded } from "@/components/Bounded";
+import { PrismicNextLink } from "@prismicio/next";
+import { Heading } from "@/components/Heading";
+import { SkateboardProduct } from "./SkateboardProduct";
+
+/**
+ * Props for `ProductGrid`.
+ */
+export type ProductGridProps = SliceComponentProps<Content.ProductGridSlice>;
+
+/**
+ * Component for "ProductGrid" Slices.
+ */
+const ProductGrid: FC<ProductGridProps> = ({ slice }) => {
+  return (
+    <Bounded
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation} 
+      className="bg-texture bg-brand-gray"
+      >
+<Heading className="text-center ~mb-4/6" as="h2">
+<PrismicText field={slice.primary.heading} />
+</Heading>
+<div className="text-center ~mb-6/10">
+<PrismicRichText field={slice.primary.body} />
+</div>
+<div className="grid w-full grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
+
+{slice.primary.product.map(({skateboard}) => 
+
+isFilled.contentRelationship(skateboard) && (
+  <SkateboardProduct key={skateboard.id} id={skateboard.id} />
+  
+))}
+</div>
+      {/**
+       * 💡 Use Prismic MCP with your code editor
+       *
+       * Get AI-powered help to build your slice components — based on your actual model.
+       *
+       * ▶️ Setup:
+       * 1. Add a new MCP Server in your code editor:
+       *
+       * {
+       *   "mcpServers": {
+       *     "Prismic MCP": {
+       *       "command": "npx",
+       *       "args": ["-y", "@prismicio/mcp-server"]
+       *     }
+       *   }
+       * }
+       *
+       * 2. Select Claude 3.7 Sonnet (recommended for optimal output)
+       *
+       * ✅ Then open your slice file and ask your code editor:
+       *    "Code this slice"
+       *
+       * Your code editor reads your slice model and helps you code faster ⚡
+       * 📚 Give your feedback: https://community.prismic.io/t/help-us-shape-the-future-of-slice-creation/19505
+       */}
+    </Bounded>
+  );
+};
+
+export default ProductGrid;
